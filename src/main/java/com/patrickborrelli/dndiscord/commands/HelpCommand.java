95861e8de0c97ad537adb28f2786c94078e3c4d2
Patrick Borrelli <patrickborrelli@gmail.com>
2020-05-22 16:53:15 -0400

    additions to Help Command and Embed framework

--- src/main/java/com/patrickborrelli/dndiscord/commands/HelpCommand.java
+++ src/main/java/com/patrickborrelli/dndiscord/commands/HelpCommand.java
@@ -1,7 +1,10 @@
 package com.patrickborrelli.dndiscord.commands;
 
+import java.awt.Color;
+
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 
 import com.patrickborrelli.dndiscord.exceptions.CommandProcessingException;
 import com.patrickborrelli.dndiscord.messaging.MessageResponse;
@@ -23,10 +26,22 @@ public class HelpCommand implements CommandExecutor {
 			MessageResponse.sendReply(channel, "Too many arguments provided");
 		} else if(args.length == 1) {
 			//send generic help response
+			buildBasicHelpEmbed(channel);
 		} else if(args.length == 2) {
 			//specific command help
 		} else {
 			MessageResponse.sendReply(channel, "Unknown argument provided.");
 		}
+	}
+	
+	private void buildBasicHelpEmbed(TextChannel channel) {
+		EmbedBuilder embed = new EmbedBuilder()
+			.setTitle("DnDiscord Help")
+			.setDescription("DnDiscord is a multifaceted D&D 5e utility bot designed to enable you and your party a seamless online D&D experience.")
+		    .setAuthor("Patrick Borrelli", "https://github.com/patrickborrelli", "https://cdn.discordapp.com/embed/avatars/0.png")
+		    .addField("Basic Commands", "For help on specific commands, type <prefix>help <command>.")
+		    .setColor(Color.GREEN)
+		    .setFooter("Footer", "https://cdn.discordapp.com/embed/avatars/1.png");
+		MessageResponse.sendEmbedMessage(channel, embed);		
 	}
 }
--- src/main/java/com/patrickborrelli/dndiscord/messaging/MessageResponse.java
+++ src/main/java/com/patrickborrelli/dndiscord/messaging/MessageResponse.java
@@ -1,6 +1,7 @@
 package com.patrickborrelli.dndiscord.messaging;
 
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 
 /**
  * Centralized provider to handle all outgoing 
@@ -12,5 +13,9 @@ import org.javacord.api.entity.channel.TextChannel;
 public class MessageResponse {
 	public static void sendReply(TextChannel channel, String reply) {
 		channel.sendMessage(reply);
+	}
+	
+	public static void sendEmbedMessage(TextChannel channel, EmbedBuilder embed) {
+		channel.sendMessage(embed);
 	}
 }
