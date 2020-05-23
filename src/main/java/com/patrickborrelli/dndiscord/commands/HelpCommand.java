02c401ec75f245e14854125b42e9ba10801d0cb3
Patrick Borrelli <patrickborrelli@gmail.com>
2020-05-23 00:50:28 -0400

    additional help embed and prefix help

--- src/main/java/com/patrickborrelli/dndiscord/commands/HelpCommand.java
+++ src/main/java/com/patrickborrelli/dndiscord/commands/HelpCommand.java
@@ -1,13 +1,17 @@
 package com.patrickborrelli.dndiscord.commands;
 
 import java.awt.Color;
+import java.net.URL;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.ApplicationInfo;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.user.User;
 
 import com.patrickborrelli.dndiscord.exceptions.CommandProcessingException;
 import com.patrickborrelli.dndiscord.messaging.MessageResponse;
@@ -22,6 +26,14 @@ import com.patrickborrelli.dndiscord.utilities.AppUtil;
 public class HelpCommand implements CommandExecutor {
 	
 	private AppUtil instance;
+	private static final String HELP_TEXT = 
+			"__**prefix**__ - sets the bot's prefix for this server.\n" +
+			"__**ping**__ - tests the bot's connection.\n" +
+			"__**help**__ - shows this message.\n";
+	
+	private static final String MORE_HELP = 
+			"An underlined command indicates that the command has subcommands.\n" +
+			"Type help <command> for more information on a specific command.";
 	
 	public HelpCommand() {
 		instance = AppUtil.getInstance();
@@ -47,14 +59,18 @@ public class HelpCommand implements CommandExecutor {
 	private void buildBasicHelpEmbed(Message msg) {
 		//get bot avatar:
 		DiscordApi apiConnection = instance.getApi();
-		CompletableFuture<ApplicationInfo> botUser = apiConnection.getApplicationInfo();
+		User botUser = apiConnection.getYourself();		
+		URL avatarUrl = botUser.getAvatar().getUrl();
+				
 		EmbedBuilder embed = new EmbedBuilder()
 			.setTitle("DnDiscord Help")
 			.setDescription("DnDiscord is a multifaceted D&D 5e utility bot designed to enable you and your party a seamless online D&D experience.")
-		    .setAuthor("Patrick Borrelli", "http://github.com/patrickborrelli", "https://cdn.discordapp.com/embed/avatars/0.png")
-		    .addField("Basic Commands", "For help on specific commands, type <prefix>help <command>.")
+		    .setAuthor("DnDiscord", "http://github.com/patrickborrelli", avatarUrl.toString())
+		    .addField("Basic Commands", HELP_TEXT)
+		    .addField("More Help", MORE_HELP)
 		    .setColor(Color.GREEN)
-		    .setFooter("Footer", "https://cdn.discordapp.com/embed/avatars/1.png");
+		    .setFooter("Footer", "https://cdn.discordapp.com/embed/avatars/1.png")
+		    .setThumbnail(avatarUrl.toString());
 		MessageResponse.sendEmbedMessage(msg.getChannel(), embed);		
 	}
 }
--- src/main/java/com/patrickborrelli/dndiscord/commands/PrefixCommand.java
+++ src/main/java/com/patrickborrelli/dndiscord/commands/PrefixCommand.java
@@ -1,17 +1,21 @@
-/**
- * 
- */
 package com.patrickborrelli.dndiscord.commands;
+
+import java.awt.Color;
+import java.net.URL;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.user.User;
 
 import com.patrickborrelli.dndiscord.DNDiscordMain;
 import com.patrickborrelli.dndiscord.exceptions.CommandProcessingException;
 import com.patrickborrelli.dndiscord.exceptions.MissingEnvironmentVarException;
 import com.patrickborrelli.dndiscord.messaging.MessageResponse;
+import com.patrickborrelli.dndiscord.utilities.AppUtil;
 import com.patrickborrelli.dndiscord.utilities.ConfigurationUtil;
 
 //TODO: will need to implement a database system so that user/server prefix can be stored in case app goes down
@@ -24,33 +28,52 @@ import com.patrickborrelli.dndiscord.utilities.ConfigurationUtil;
  */
 public class PrefixCommand implements CommandExecutor {
 	private static final Logger LOGGER = LogManager.getLogger(DNDiscordMain.class);
-	private static ConfigurationUtil instance;
+	private static ConfigurationUtil configUtil;
+	private AppUtil appUtil;
 	
 	public PrefixCommand() {
 		try {
-			instance = ConfigurationUtil.getInstance();
+			configUtil = ConfigurationUtil.getInstance();
 		} catch(MissingEnvironmentVarException mevEx) {
 			System.err.println("Failed to initiate DNDiscord. " + mevEx.getLocalizedMessage());
 			mevEx.printStackTrace();
 			LOGGER.error("Failed to initiate DNDiscord. " + mevEx.getLocalizedMessage());
 		}
+		appUtil = AppUtil.getInstance();
 	}
 
 	@Override
 	public void onCommand(Message msg) throws CommandProcessingException {
 		TextChannel channel = msg.getChannel();
 		String[] args = msg.getContent().split(" ");
-		String currentPrefix = instance.getBotPrefix();
+		String currentPrefix = configUtil.getBotPrefix();
 		
 		if(args.length > 2) {
-			MessageResponse.sendReply(channel, "Too many arguments provided");
+			buildFormatEmbed(msg);
 		} else if(args.length == 1) {
-			//TODO: send command help
-			MessageResponse.sendReply(channel, "You are doing it wrong!");
+			buildFormatEmbed(msg);
 		} else if(args.length == 2) {
-			//get args[1] and set it as the prefix
-			instance.setBotPrefix(args[1]);			
-			MessageResponse.sendReply(channel, "DnDiscord command prefix changed from: " + currentPrefix + " to: " + instance.getBotPrefix());
+			if(args[1].equalsIgnoreCase("list")) {
+				MessageResponse.sendReply(channel, "Current prefix is: " + configUtil.getBotPrefix());
+			} else {
+				//get args[1] and set it as the prefix
+				configUtil.setBotPrefix(args[1]);			
+				MessageResponse.sendReply(channel, "DnDiscord command prefix changed from: " + currentPrefix + " to: " + configUtil.getBotPrefix());
+			}			
 		}
+	}
+	
+	private void buildFormatEmbed(Message msg) {
+		//get bot avatar:
+		DiscordApi apiConnection = appUtil.getApi();
+		User botUser = apiConnection.getYourself();		
+		URL avatarUrl = botUser.getAvatar().getUrl();
+		
+		EmbedBuilder embed = new EmbedBuilder()
+			.setTitle("Prefix Command Help")
+		    .addField("Command Format", "prefix <<new prefix characters>> || prefix list\n")
+		    .setColor(Color.GREEN)
+		    .setThumbnail(avatarUrl.toString());
+		MessageResponse.sendEmbedMessage(msg.getChannel(), embed);		
 	}
 }
